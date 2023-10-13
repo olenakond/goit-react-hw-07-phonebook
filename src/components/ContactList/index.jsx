6ec83851ad7f7bab881409fa3e9400629f90b6e6
Olena <shapovalev@gmail.com>
2023-10-13 12:55:28 +0200

    add-createSelector

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,23 +1,10 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilterValue } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 import Contact from '../Contact';
 import { List } from './ContactList.styled';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilterValue);
-
-  const createContactsByfilter = () => {
-    if (!filter) {
-      return;
-    }
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  const filteredContacts = createContactsByfilter();
-  const contactsList = filteredContacts ?? contacts;
+  const contactsList = useSelector(selectFilteredContacts);
 
   return (
     <>
